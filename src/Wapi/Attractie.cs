93bd4d24ec0585370675fad26898767dfd2d50d3
Justwouter <wouter2312@gmail.com>
2022-10-06 10:44:45 +0200

    Doesn't work just stashing

---
+++ src/Wapi/Attractie.cs
@@ -0,0 +1,11 @@
+namespace Wapi;
+
+public class Attractie{
+    public int Id { get; set; }
+    public string Naam { get; set; }
+    public int Engheid {get;set;}
+
+    public DateTime BouwJaar {get;set;}
+
+    public long AantalLikes {get;set;}
+}
--- src/Wapi/Program.cs
+++ src/Wapi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<SchoolContext>()
+                .AddDefaultTokenProviders();
+builder.Services.AddAuthentication()
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
