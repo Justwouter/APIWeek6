3c9d0479638f80f8b2b09a25b4f79d0ed3831fe1
Justwouter <wouter2312@gmail.com>
2022-10-12 14:36:38 +0200

    Attempt #2

--- src/ExampleAPI/Data/SchoolContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-
-    public class SchoolContext : IdentityDbContext
-    {
-        public SchoolContext (DbContextOptions<SchoolContext> options) : base(options){}
-
-        public DbSet<Vak> Vak { get; set; } = default!;
-    }
